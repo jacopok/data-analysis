74a7ec79fb32a92fd1fb55e254f4289587e8247e
Jacopo Tissino <jacopok@gmail.com>
2018-11-13 08:58:01 +0100

    Added plotting functions for data fitting class, added first experiment for calculating goodness of fit.

--- fits_multimeter_errors.py
+++ fits_multimeter_errors.py
@@ -1,16 +1,18 @@
 import numpy as np
 import scipy
 from scipy import optimize
+import scipy.stats as sps
 import matplotlib.pyplot as plt
 
 class dataset:
     def __init__(self, x_array, y_array, x_type, y_type):
         self.x_array = x_array
         self.y_array = y_array
-        self.x_type = x_type # 'a' or 'v', for current or tension
+        self.x_type = x_type
         self.y_type = y_type
         self.x_error_array = []
         self.y_error_array = []
+        self.y_prediction = []
     
     def calculate_error(self, scale_array, axis, multimeter_type):
         if (axis=='x'):
@@ -21,11 +23,11 @@ class dataset:
             data_type = self.y_type
         else:
             print("axis must be x or y")
-            return(0)
+            return(None)
         
         if (len(scale_array) != len(array)):
             print("scale_array must be have the same length as the data array")
-            return(0)
+            return(None)
         else:
             n = len(scale_array)
         
@@ -42,11 +44,11 @@ class dataset:
         
         return(0)
         
-    def model(x, params):
-        y = x * params[0] + params[1]
+    def model(self, x, a, b):
+        y = x * a + b
         return(y)
         
-    def fit (self, initial_params=False):
+    def fit (self, initial_params=None):
         """
         fits the model
         
@@ -54,17 +56,70 @@ class dataset:
         """
         
         params, pcov = scipy.optimize.curve_fit(self.model, self.x_array, self.y_array,
-                                      sigma=self.y_error_array, absolute_sigma=True)
+                                      sigma=self.y_error_array, p0=initial_params, absolute_sigma=True)
         
         error_params = []
         for i in range(np.shape(pcov)[0]):
             error_params.append(pcov[i,i])
         
         return(params, error_params)
+    
+    def calculate_residuals(self):
+        params, error_params = self.fit()
+        self.calculate_y_prediction()
+        y_array = np.array(self.y_array)
+        residuals = y_array - self.y_prediction
+        return(residuals)
+    
+    def calculate_y_prediction(self):
+        params, error_params = self.fit()
+        y_model = []
+        for x in self.x_array:
+            y_model.append(self.model(x, *params))
+        self.y_prediction = np.array(y_model)
+
         
-    #def plot (self):
+    def goodness_of_fit(self):
+        self.calculate_y_prediction()
+        chisq, pvalue = sps.chisquare(self.y_array, self.y_prediction)
+        return([chisq, pvalue])
         
+    def full_plot(self):
+        """Plots the data with the corresponding errors, along with the model
         
+        """
+        
+        if(len(self.y_error_array) != len(self.y_array)):
+            print('Must calculate errors first')
+            return(None)
+        
+        #Create gradual x values for plotting
+        n_steps = 1000
+        percent_plot_excess = 0.1
+        x_min = np.min(self.x_array)
+        x_max = np.max(self.x_array)
+        x_step = (x_max - x_min) / n_steps
+        x_excess = n_steps * percent_plot_excess * x_step
+        x_range = np.arange(x_min-x_excess, x_max+x_excess, x_step)
+        
+        params, error_params = self.fit()
+        y_range = self.model(x_range, params[0], params[1])
+        
+        plt.plot(x_range, y_range, '--')
+        plt.errorbar(x=self.x_array, y=self.y_array, yerr=self.y_error_array, fmt = 'bo')
+        
+        plt.show()
+    
+    def residuals_plot(self):
+        residuals = self.calculate_residuals()
+        if(len(self.y_error_array) != len(residuals)):
+            print('Must calculate errors first')
+            return(0)
+            
+        plt.plot(self.x_array, np.zeros(len(residuals)), '--')
+        plt.errorbar(self.x_array, residuals, self.y_error_array, fmt = 'bo')
+        plt.show()
+    
 def multimeter_error(value, scale, multimeter_type, measure_type):
     """
     value is the value measured by the multimeter
@@ -96,7 +151,7 @@ def multimeter_error(value, scale, multimeter_type, measure_type):
             digit_accuracy = 3 * np.array([1, 1, 1, 1, 1, 1])
         else:
             print(f'{measure_type} is not a valid measure type')
-            return(0)
+            return(None)
     elif(multimeter_type == 'm'):
         if(measure_type == 'v'):
             scale_array = np.array([1, 10, 100, 1000])
@@ -120,24 +175,24 @@ def multimeter_error(value, scale, multimeter_type, measure_type):
             digit_accuracy = np.array([15, 8, 8, 10, 10, 10, 15, 15])
         else:
             print(f'{measure_type} is not a valid measure type')
-            return(0)
+            return(None)
     else:
         print(f'{multimeter_type} is not a valid multimeter type')
-        return(0)
+        return(None)
     
     if(not(len(scale_array) == len(resolution_array)
     == len(percent_accuracy) == len(digit_accuracy))):
         print('Error in hardcoded values')
-        return(0)
+        return(None)
     
-    tolerance = 0.1
+    tolerance = 0.01
     index = -1
     for i in range(len(scale_array)):
         if(np.abs(np.log(scale_array[i]) - np.log(scale)) < tolerance):
             index = i
     if(index == -1):
         print(f'{scale} is an invalid scale')
-        return(0)
+        return(None)
     
     print(index)
     error = (percent_accuracy[index] * value / 100 
@@ -145,3 +200,7 @@ def multimeter_error(value, scale, multimeter_type, measure_type):
     
     return(error)
     
+def test_initialization():
+    global test
+    test = dataset([1, 2, 3], [1, 1.6, 2], 'v', 'a')
+    test.y_error_array = [0.1, 0.05, 0.2]
