cb69cd4080cf31836dca85f16765ca62651aa2a8
Jacopo Tissino <jacopok@gmail.com>
2018-11-14 15:43:40 +0100

    Added data, and a couple utility functions for easier utilization of the main ones. Also, compatibility function.

--- fits_multimeter_errors.py
+++ fits_multimeter_errors.py
@@ -5,16 +5,19 @@ import scipy.stats as sps
 import matplotlib.pyplot as plt
 from uncertainties import *
 from uncertainties.umath import *
+from uncertainties import unumpy
 
 class dataset:
     def __init__(self, x_array, y_array, x_type, y_type):
         self.x_array = np.array(x_array)
+        self.original_n = len(self.x_array)
         self.y_array = np.array(y_array)
         self.x_type = x_type
         self.y_type = y_type
         self.x_error_array = np.array([])
         self.y_error_array = np.array([])
         self.y_prediction = np.array([])
+        self.crop = 0
     
     def calculate_error(self, scale_array, axis, multimeter_type):
         if (axis=='x'):
@@ -46,6 +49,13 @@ class dataset:
         
         return(0)
         
+    def crop_dataset(self):
+        self.n = self.original_n - self.crop
+        self.x_array = self.x_array[:self.n]
+        self.y_array = self.y_array[:self.n]
+        self.x_error_array = self.x_error_array[:self.n]
+        self.y_error_array = self.y_error_array[:self.n]
+        
     def model(self, x, a, b):
         y = x * a + b
         return(y)
@@ -56,6 +66,7 @@ class dataset:
         
         returns params, error_params
         """
+        self.crop_dataset()
         
         params, pcov = scipy.optimize.curve_fit(self.model, self.x_array, self.y_array,
                                       sigma=self.y_error_array, p0=initial_params, absolute_sigma=True)
@@ -69,14 +80,13 @@ class dataset:
     def calculate_residuals(self):
         params, error_params = self.fit()
         self.calculate_y_prediction()
-        y_array = np.array(self.y_array)
-        residuals = y_array - self.y_prediction
+        residuals = self.y_array - self.y_prediction
         return(residuals)
     
     def calculate_y_prediction(self):
         params, error_params = self.fit()
         y_model = []
-        for x in self.x_array:
+        for x in self.x_array[:self.n]:
             y_model.append(self.model(x, *params))
         self.y_prediction = np.array(y_model)
 
@@ -90,6 +100,7 @@ class dataset:
         """Plots the data with the corresponding errors, along with the model
         
         """
+        self.crop_dataset()
         
         if(len(self.y_error_array) != len(self.y_array)):
             print('Must calculate errors first')
@@ -113,11 +124,12 @@ class dataset:
         plt.show()
     
     def residuals_plot(self):
+        self.crop_dataset()
         residuals = self.calculate_residuals()
         if(len(self.y_error_array) != len(residuals)):
             print('Must calculate errors first')
             return(0)
-            
+        
         plt.plot(self.x_array, np.zeros(len(residuals)), '--')
         plt.errorbar(self.x_array, residuals, self.y_error_array, fmt = 'bo')
         plt.show()
@@ -165,17 +177,17 @@ def multimeter_error(value, scale, multimeter_type, measure_type, ignore_gain = 
             resolution_array = float(10)**(np.array([-5, -4, -3, -2]))
             percent_accuracy = np.array([0.05, 0.03, 0.03, 0.035])
             digit_accuracy = 8 * np.array([1, 1, 1, 1])
-        if(measure_type == 'a'):
+        elif(measure_type == 'a'):
             scale_array = float(10)**(np.array([-3, -2, -1, 0, 1]))
             resolution_array = float(10)**(np.array([-3, -2, -1, 0, 1] - 5))
             percent_accuracy = np.array([0.1, 0.08, 0.08, 0.15, 0.5])
             digit_accuracy = np.array([15, 8, 8, 8, 15])
-        if(measure_type == 'ohm'):
+        elif(measure_type == 'ohm'):
             scale_array = float(10)**(np.array([3, 4, 5, 6, 7, 8]))
             resolution_array = float(10)**(np.array([3, 4, 5, 6, 7, 8]) - 5)
             percent_accuracy = np.array([0.1, 0.07, 0.07, 0.07, 1, 3])
             digit_accuracy = np.array([8, 8, 8, 8, 80, 80])
-        if(measure_type == 'c'):
+        elif(measure_type == 'c'):
             scale_array = float(10)**(np.array([-9, -8, -7, -6, -5, -4, -3, -2]))
             resolution_array = float(10)**(np.array([-9, -8, -7, -6, -5, -4, -3, -2]) - 3)
             percent_accuracy = np.array([2.5, 1, 1, 1, 1, 1, 1, 1.5])
@@ -185,7 +197,8 @@ def multimeter_error(value, scale, multimeter_type, measure_type, ignore_gain = 
             return(None)
     elif(multimeter_type == 'o'):
         if(measure_type == 'v'):
-            scale_array = float(10)**(np.arange(-9, 2))
+            mag_ord = float(10)**(np.arange(-9, 2))
+            scale_array = np.concatenate((mag_ord, 2*mag_ord, 5*mag_ord))
             resolution_array = scale_array/10
             percent_accuracy = 0.01 * np.ones(len(scale_array))
             digit_accuracy = np.ones(len(scale_array))
@@ -238,7 +251,28 @@ def multimeter_error_array(value, scale, multimeter_type, measure_type, ignore_g
     
     for i in range(len(value)):
         error = multimeter_error(value[i], scale[i], multimeter_type, measure_type, ignore_gain)
-        entry = np.array(ufloat(value[i], error))
+        entry = ufloat(value[i], error)
         errors_array[i] = entry
         
     return(errors_array)
+    
+def ufloat_single_value(value, scale, multimeter_type, measure_type, ignore_gain=False):
+    return(ufloat(value,
+                  float(multimeter_error(value, scale, multimeter_type, measure_type, ignore_gain))))
+    
+def ufloat_compatibility(x, y):
+    diff = np.abs(x.n - y.n)
+    error = (x - y).s
+    return(diff/error)
+    
+def uarray_compatibility(x, y):
+    if(len(x) != len(y)):
+        print("x and y must be of the same size")
+        return(None)
+        
+    n = len(x)
+    compatibility = np.zeros(n)
+    for i in range(n):
+        compatibility[i] = ufloat_compatibility(x[i], y[i])
+    return(compatibility)
+        
--- opamp_load_data.py
+++ opamp_load_data.py
@@ -6,26 +6,33 @@ def test_initialization():
     test = dataset([1, 2, 3], [1, 1.6, 2], 'v', 'a')
     test.y_error_array = [0.1, 0.05, 0.2]
     
+# alimentation capacities
+C_arr_0 = 10**(-9) * np.array([98.2, 89.0])
+C_scale_arr_0 = 10**(-7) * np.ones(2)
+
+C_0 = multimeter_error_array(C_arr_0, C_scale_arr_0, 'm', 'c')
+
 # 1: MISURA DELLA CURVA DI TRASFERIMENTO PER UN AMPLIFICATORE INVERTENTE 
 
-R_arr_1 = np.array([])
-R_scale_arr_1 = np.array([])
+R_arr_1 = np.array([5612.2, 32624, 46.79])
+R_scale_arr_1 = np.array([10000, 100000, 1000])
 
 R_1 = multimeter_error_array(R_arr_1, R_scale_arr_1, 'm', 'ohm', ignore_gain=True)
-#contains R1, R2, R3 with uncertainties. CHECH MULT TYPE
+#contains R1, R2, R3 with uncertainties 
 
 m_theory_1 = - R_1[1] / R_1[0]
 
-freq_1 = ufloat(, 0)
+freq_1 = ufloat(1000, 0) # Hz
 
-Vin_plus_1 = np.array([])
-Vout_plus_1 = np.array([])
-Vin_minus_1 = np.array([])
-Vout_minus_1 = np.array([])
-Vin_plus_vdiv_1 = np.array([])
-Vout_plus_vdiv_1 = np.array([])
-Vin_minus_vdiv_1 = np.array([])
-Vout_minus_vdiv_1 = np.array([])
+Vin_plus_1 = np.array([0.100, 0.292, .536, .780, 1.02, 1.26, 1.5, 1.76, 2, 2.48, 2.88])
+Vout_plus_1 = np.array([0.592, 1.76, 3.20, 4.72, 6.08, 7.80, 9.4, 10.8, 12.2, 14.8, 15.2])
+Vin_minus_1 = np.array([-0.096, -0.284, -.528, -.780, -1.02, -1.26, -1.5, -1.72, -1.96, -2.44, -2.88])
+Vout_minus_1 = np.array([-0.568, -1.72, -3.16, -4.72, -6, -7.60, -9, -10.4, -11.8, -14, -14.2])
+
+Vin_plus_vdiv_1 = np.array([0.05, 0.1, 0.2, 0.5, 0.5, 0.5, 0.5, 1, 1, 1, 1])
+Vout_plus_vdiv_1 = np.array([0.2, 1, 1, 2, 2, 5, 5, 5, 5, 5, 5])
+Vin_minus_vdiv_1 = Vin_plus_vdiv_1 # the resolution is the same since it's the same scale
+Vout_minus_vdiv_1 = Vout_plus_vdiv_1
 
 transfer_plus_1 = dataset(Vin_plus_1, Vout_plus_1, 'v', 'v')
 transfer_minus_1 = dataset(Vin_minus_1, Vout_minus_1, 'v', 'v')
@@ -35,72 +42,81 @@ transfer_plus_1.calculate_error(Vout_plus_vdiv_1, 'y', 'o')
 transfer_minus_1.calculate_error(Vin_minus_vdiv_1, 'x', 'o')
 transfer_minus_1.calculate_error(Vout_minus_vdiv_1, 'y', 'o')
 
+
 # 2: AMPLIFICATORE DELLE DIFFERENZE – AMPLIFICATORE NON INVERTENTE
 
-R_arr_21 = np.array([])
-R_scale_arr_21 = np.array([])
+R_arr_21 = np.array([5613.3, 32515, 46.64])
+R_scale_arr_21 = np.array([10000, 100000, 1000])
 
 R_21 = multimeter_error_array(R_arr_21, R_scale_arr_21, 'm', 'ohm', ignore_gain=True)
 #contains R1, R2, R3 (b) 
 
-freq_21 = ufloat(, 0)
+freq_21 = ufloat(1000, 0)
 
-#Non-invertent: 2.1
+#Non-invertent: 2.1a
 
-Vin_arr_21 = np.array([])
-Vin_scale_arr_21 = np.array([])
-Vout_arr_21  = np.array([])
-Vout_scale_arr_21 = np.array([])
+Vin_arr_21 = np.array([.196, .984, 1.94])
+Vin_scale_arr_21 = np.array([.05, .2, .5])
+Vout_arr_21  = np.array([1.16, 5.8, 11.6])
+Vout_scale_arr_21 = np.array([.2, 1, 2])
 
-Vin_21 = multimeter_error_array(Vin_arr_21, Vin1_scale_arr_21, '', 'v')
-Vout_21 = multimeter_error_array(Vout_arr_21, Vout_scale_arr_21, '', 'v')
+Vin_21 = multimeter_error_array(Vin_arr_21, Vin_scale_arr_21, 'o', 'v')
+Vout_21 = multimeter_error_array(Vout_arr_21, Vout_scale_arr_21, 'o', 'v')
 
-#Amp differences: 2.1
+A_21 = Vout_21 / Vin_21
 
-Vin1_arr_21 = np.array([])
-Vin1_scale_arr_21 = np.array([])
-Vin2_arr_21 = np.array([])
-Vin2_scale_arr_21 = np.array([])
-Vout1_arr_21 = np.array([]) 
-Vout1_scale_arr_21 = np.array([])
+A_teor_21 = R_1[1]/R_1[0] * np.ones(len(A_21))
 
-Vin1_21 = multimeter_error_array(Vin1_arr_21, Vin1_scale_arr_21, '', 'v')
-Vin2_21 = multimeter_error_array(Vin2_arr_21, Vin2_scale_arr_21, '', 'v')
-Vout1_21 = multimeter_error_array(Vout1_arr_21, Vout1_scale_arr_21, '', 'v')
+compatibility_21a = uarray_compatibility(A_21, A_teor_21)
+
+#Amp differences: 2.1b
+
+Vin1_arr_21 = np.array([-.284, .292, .292, -.284])
+Vin1_scale_arr_21 = 0.1 * np.ones(4)
+Vin2_arr_21 = np.array([.488, .488, -.480, -.480])
+Vin2_scale_arr_21 = 0.2 * np.ones(4)
+Voutb_arr_21 = np.array([4.56, 1.2, -4.4, -1.12]) 
+Voutb_scale_arr_21 = 2 * np.ones(4)
+
+Vin1_21 = multimeter_error_array(Vin1_arr_21, Vin1_scale_arr_21, 'o', 'v')
+Vin2_21 = multimeter_error_array(Vin2_arr_21, Vin2_scale_arr_21, 'o', 'v')
+Voutb_21 = multimeter_error_array(Voutb_arr_21, Voutb_scale_arr_21, 'o', 'v')
+
+Vout_teor_21 = R_1[1] / R_1[0] * (Vin2_21 - Vin1_21)
+
+compatibility_21b = uarray_compatibility(Vout_teor_21, Voutb_21)
 
 #Amp sums invertent: 2.2
 
-R_arr_22 = np.array([])
-R_scale_arr_22 = np.array([])
+Rf_22 = ufloat_single_value(5621.6, 100000, 'm', 'ohm')
+#contains Rf
+#also, use R1 from 1 and R1b from 21
 
-R_22 = multimeter_error_array(R_arr_22, R_scale_arr_22, 'm', 'ohm', ignore_gain=True)
-#contains Rf, R1, R1b
+Vin1_arr_22 = np.array([-.284, .292, .292, -.284])
+Vin1_scale_arr_22 = 0.1 * np.ones(4)
+Vin2_arr_22 = np.array([.488, .488, -.480, -.480])
+Vin2_scale_arr_22 = 0.2 * np.ones(4)
+Vout1_arr_22 = np.array([-.180, -.760, .200, .780]) 
+Vout1_scale_arr_22 = 0.5 * np.ones(4)
 
-Vin1_arr_22 = np.array([])
-Vin1_scale_arr_22 = np.array([])
-Vin2_arr_22 = np.array([])
-Vin2_scale_arr_22 = np.array([])
-Vout1_arr_22 = np.array([]) 
-Vout1_scale_arr_22 = np.array([])
+Vin1_22 = multimeter_error_array(Vin1_arr_22, Vin1_scale_arr_22, 'o', 'v')
+Vin2_22 = multimeter_error_array(Vin2_arr_22, Vin2_scale_arr_22, 'o', 'v')
+Vout1_22 = multimeter_error_array(Vout1_arr_22, Vout1_scale_arr_22, 'o', 'v')
 
-Vin1_22 = multimeter_error_array(Vin1_arr_22, Vin1_scale_arr_22, '', 'v')
-Vin2_22 = multimeter_error_array(Vin2_arr_22, Vin2_scale_arr_22, '', 'v')
-Vout1_22 = multimeter_error_array(Vout1_arr_22, Vout1_scale_arr_22, '', 'v')
-
+Vout_teor_22 = - (Vin1_22 + Vin2_22)
+compatibility_22 = uarray_compatibility(Vout_teor_22, Vout1_22)
 
 #Integrator for impulses: 2.3
 
-C_arr_23 = np.array([])
-C_scale_arr_23 = np.array([])
+C_arr_23 = 10**(-12) * np.array([340, 646]) 
+C_scale_arr_23 = 10**(-12) * np.array([1000, 1000])
 
-C_23 = multimeter_error_array(C_arr_23, C_scale_arr_23, '', 'c', ignore_gain=True)
+C_23 = multimeter_error_array(C_arr_23, C_scale_arr_23, 'm', 'c', ignore_gain=True)
 #Contains Cf, C1
 
-freq_23 = ufloat(, 0)
+freq_23 = ufloat(50, 0)
 
-Rf_val_23 = 
-Rf_scale_23 = 
-Rf_23 = ufloat(Rf_val_23, multimeter_error(Rf_val_23, Rf_scale_23, '', 'ohm'))
+Rf_23 = ufloat_single_value(1.0008 * 10**(6), 10**7, 'm', 'ohm')
 
 Vin_23 = np.array([])
 Vout_23 = np.array([])
@@ -112,4 +128,3 @@ transfer_23.calculate_error(Vin_vdiv_23, 'x', 'o')
 transfer_23.calculate_error(Vout_vdiv_23, 'y', 'o')
 
 # RADDRIZZATORE DI PRECISIONE CON OPERAZIONALI: 3
-
