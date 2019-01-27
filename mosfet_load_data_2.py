Corrected slight mistakes in figure labels, added computation of a couple variables we'd forgot.

mosfet_load_data_2.py
@@ -18,16 +18,17 @@ V_DS_SPICE_21 = [5, 0.145]
 
 V_DS_21 = multimeter_error_array(V_DS_arr_21, V_DS_scale_arr_21, 'a', 'v')
 
-V_DS_th_21 = V_DD_21 - V_GS_21 #??? forse sbagliato
+V_DS_th_21 = V_DD_21 - V_GS_21 #inizializzazione di base, poi sistemata
+R_on_21 = 1/k_n_13 /(V_GS_21[1] - V_TN_13)
 
-
+V_DS_th_21[1] = V_DD_21 * R_on_21 / (R_on_21 + R[1])
 
 M_21 = np.stack((V_GS_21, V_GS_scale_arr_21, V_DS_21,
                  V_DS_scale_arr_21, V_DS_SPICE_21)).T
 
 col_names_21 = "$V_{GS}$ & v/div & $V_{DS}$ & v/div & $V_{DS}^{SPICE}$"
 
-#print_matrix(M_21, col_names_21)
+#print_matrix(M_21, col_names_21, uniform_units="V")
 
 lambda_array_21 = []
 

mosfet_load_data_3.py
@@ -119,7 +119,7 @@ Vars_36 = np.array([V_in_36, V_out_36, V_in_noload_36, V_out_noload_36,
                     R_out_36, R_out_cd_36])
 Units_36 = ["V", "V", "V", "V", "", "", "", "", "\Omega", "\Omega"]
 
-print_ufloat_array(Names_36, Vars_36, Units_36)
+#print_ufloat_array(Names_36, Vars_36, Units_36)
 
-ufloat_compatibility(A_exp_36, A_teor_36, True)
-ufloat_compatibility(A_exp_noload_36, A_teor_noload_36, True)
+#ufloat_compatibility(A_exp_36, A_teor_36, True)
+#ufloat_compatibility(A_exp_noload_36, A_teor_noload_36, True)

mosfet_main.py
@@ -1,11 +1,11 @@
 from fits_multimeter_errors import *
 from mosfet_load_data_4 import *
 
-characteristic_12.full_plot('$V_{ds}$', '$I_d$', 'p12completo')
-characteristic_12.residuals_plot('Vds-Vteor', '$I_d$', 'p12residui')
+characteristic_12.full_plot('$V_{ds}$ [V]', '$I_d$ [A]', 'p12completo')
+characteristic_12.residuals_plot('$V_{ds}$ [V]', '$I_d - I_d^{fit}$ [A]', 'p12residui')
 
-characteristic_gs_13.full_plot('$V_{gs}$', '$Y$', 'p13completo')
-characteristic_gs_13.residuals_plot('$V_{gs}$', '$Y-Y_{model}$', 'p13residui')
+characteristic_gs_13.full_plot('$V_{gs}$ [V]', '$Y$ [A$^{1/2}$]', 'p13completo')
+characteristic_gs_13.residuals_plot('$V_{gs}$ [V]', '$Y-Y_{fit}$ [A$^{1/2}$]', 'p13residui')
 
 maximum_ignored_12 = 6
 values = np.empty((5, maximum_ignored_12))
@@ -79,7 +79,7 @@ for Vds in Vds_array:
     plt.plot(Vgs, I, label = ("$V_{DS} \\approx $ " + '{0:.{1}f}'.format(Vds, 1) + " V"))
 plt.errorbar(x=characteristic_gs_13.x_array, y=unumpy.nominal_values(I_d_13),
             yerr=unumpy.std_devs(I_d_13), fmt = 'bo', marker = 'x',
-            label = "Experimental data")
+            label = "Dati sperimentali")
 
 Imax = Id_v(Vds_array[-1], Vgs[-1], V_TN_13.n, k_n_13.n, lambda_n_12.n)
 